bcc37117e57dd94b20588b5e4ff1ec2b210d4983
robinhelf <100934574+robinhelf@users.noreply.github.com>
2025-02-26 15:35:15 +0800

    Update patch.py
    Signed-off-by: robinhelf <100934574+robinhelf@users.noreply.github.com>

--- patch.py
+++ patch.py
@@ -291,28 +291,28 @@ def patch_squashfs(path,key_dict):
                     _data = replace_key(old_public_key,new_public_key,data,file)
                     if _data != data:
                         open(file,'wb').write(_data)
-                url_dict = {
-                    os.environ['MIKRO_LICENCE_URL'].encode():os.environ['CUSTOM_LICENCE_URL'].encode(),
-                    os.environ['MIKRO_UPGRADE_URL'].encode():os.environ['CUSTOM_UPGRADE_URL'].encode(),
-                    os.environ['MIKRO_CLOUD_URL'].encode():os.environ['CUSTOM_CLOUD_URL'].encode(),
-                    os.environ['MIKRO_CLOUD_PUBLIC_KEY'].encode():os.environ['CUSTOM_CLOUD_PUBLIC_KEY'].encode(),
-                }
-                data = open(file,'rb').read()
-                for old_url,new_url in url_dict.items():
-                    if old_url in data:
-                        print(f'{file} url patched {old_url.decode()[:7]}...')
-                        data = data.replace(old_url,new_url)
-                        open(file,'wb').write(data)
+                # url_dict = {
+                #     os.environ['MIKRO_LICENCE_URL'].encode():os.environ['CUSTOM_LICENCE_URL'].encode(),
+                #     os.environ['MIKRO_UPGRADE_URL'].encode():os.environ['CUSTOM_UPGRADE_URL'].encode(),
+                #     os.environ['MIKRO_CLOUD_URL'].encode():os.environ['CUSTOM_CLOUD_URL'].encode(),
+                #     os.environ['MIKRO_CLOUD_PUBLIC_KEY'].encode():os.environ['CUSTOM_CLOUD_PUBLIC_KEY'].encode(),
+                # }
+                # data = open(file,'rb').read()
+                # for old_url,new_url in url_dict.items():
+                #     if old_url in data:
+                #         print(f'{file} url patched {old_url.decode()[:7]}...')
+                #         data = data.replace(old_url,new_url)
+                #         open(file,'wb').write(data)
                         
-                if os.path.split(file)[1] == 'licupgr':
-                    url_dict = {
-                        os.environ['MIKRO_RENEW_URL'].encode():os.environ['CUSTOM_RENEW_URL'].encode(),
-                    }
-                    for old_url,new_url in url_dict.items():
-                        if old_url in data:
-                            print(f'{file} url patched {old_url.decode()[:7]}...')
-                            data = data.replace(old_url,new_url)
-                            open(file,'wb').write(data)
+                # if os.path.split(file)[1] == 'licupgr':
+                #     url_dict = {
+                #         os.environ['MIKRO_RENEW_URL'].encode():os.environ['CUSTOM_RENEW_URL'].encode(),
+                #     }
+                #     for old_url,new_url in url_dict.items():
+                #         if old_url in data:
+                #             print(f'{file} url patched {old_url.decode()[:7]}...')
+                #             data = data.replace(old_url,new_url)
+                #             open(file,'wb').write(data)
                     
 def run_shell_command(command):
     process = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -332,9 +332,9 @@ def patch_npk_package(package,key_dict):
         print(f"extract {squashfs_file} ...")
         run_shell_command(f"unsquashfs -d {extract_dir} {squashfs_file}")
         patch_squashfs(extract_dir,key_dict)
-        logo = os.path.join(extract_dir,"nova/lib/console/logo.txt")
-        run_shell_command(f"sudo sed -i '1d' {logo}") 
-        run_shell_command(f"sudo sed -i '8s#.*#  elseif@live.cn     https://github.com/elseif/MikroTikPatch#' {logo}")
+        # logo = os.path.join(extract_dir,"nova/lib/console/logo.txt")
+        # run_shell_command(f"sudo sed -i '1d' {logo}") 
+        # run_shell_command(f"sudo sed -i '8s#.*#  elseif@live.cn     https://github.com/elseif/MikroTikPatch#' {logo}")
         print(f"pack {extract_dir} ...")
         run_shell_command(f"rm -f {squashfs_file}")
         run_shell_command(f"mksquashfs {extract_dir} {squashfs_file} -quiet -comp xz -no-xattrs -b 256k")
